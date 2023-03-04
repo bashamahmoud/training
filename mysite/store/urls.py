174b16cfde49a1de72a49d2d2772b587ec334207
bashamahmoud <basha.mahmoud.2000@gmail.com>
2023-03-04 23:30:06 +0200

    fixing

--- mysite/store/urls.py
+++ mysite/store/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 
 from .views import CustomerView
 
-router = routers.SimpleRouter()
+router = routers.SimpleRouter()  # simple router
 router.register(r'customers', CustomerView, basename='customer')
 
 urlpatterns = [
